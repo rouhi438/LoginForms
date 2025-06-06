3a6ab75155647c432305291f92e747cfa25da54b
Abbas Rouhi Hansen <rouhi438@gmail.com>
2025-06-06 23:22:10 +0200

    Adding image and javaScript file and optimized CSS/styles of form

---
+++ LoginForm2/README.md
@@ -0,0 +1,7 @@
+## Simple SignUp Form Using only HTML & CSS
+
+---
+
+A Sign Up form with input fields styled purely with CSS.
+
+Click [here](https://rouhi438.github.io/LoginForms/LoginForm2) to view the live DEMO
---
+++ LoginForm2/script.js
@@ -0,0 +1,19 @@
+const signInBtn = document.querySelector(".btn-signin");
+const signinForm = document.querySelector(".signin-form");
+const leftBtnHolder = document.querySelector(".leftbtn-holder");
+const rightBtnHolder = document.querySelector(".rightbtn-holder");
+const signupForm = document.querySelector(".signup-form");
+const signUpBtn = document.querySelector(".btn-signup");
+
+signInBtn.addEventListener("click", function () {
+  signinForm.classList.add("left-active");
+  leftBtnHolder.classList.add("right-hidden");
+  signupForm.classList.add("right-hidden");
+});
+
+signUpBtn.addEventListener("click", function () {
+  signinForm.classList.remove("left-active");
+  rightBtnHolder.classList.add("active");
+  leftBtnHolder.classList.remove("right-hidden");
+  signupForm.classList.remove("right-hidden");
+});
