41097569f531b33a557134eb634bb51fabaaca5f
Abbas Rouhi Hansen <rouhi438@gmail.com>
2025-07-05 21:52:56 +0200

    Reset classes to fix form toggle bug

--- LoginForm2/script.js
+++ LoginForm2/script.js
@@ -6,12 +6,21 @@ const signupForm = document.querySelector(".signup-form");
 const signUpBtn = document.querySelector(".btn-signup");
 
 signInBtn.addEventListener("click", function () {
+  signinForm.className = "signin-form";
+  signupForm.className = "signup-form";
+  leftBtnHolder.className = "leftbtn-holder";
+  rightBtnHolder.className = "rightbtn-holder";
+
   signinForm.classList.add("left-active");
   leftBtnHolder.classList.add("right-hidden");
   signupForm.classList.add("right-hidden");
 });
-
 signUpBtn.addEventListener("click", function () {
+  signinForm.className = "signin-form";
+  signupForm.className = "signup-form";
+  leftBtnHolder.className = "leftbtn-holder";
+  rightBtnHolder.className = "rightbtn-holder";
+
   signinForm.classList.remove("left-active");
   rightBtnHolder.classList.add("active");
   leftBtnHolder.classList.remove("right-hidden");
